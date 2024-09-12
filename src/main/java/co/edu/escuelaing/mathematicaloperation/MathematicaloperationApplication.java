1e0d30f05476757078f791e4e9c23063dc9b2a11
unknown <johann.amaya@labinfo.is.escuelaing.edu.co>
2024-09-12 13:43:52 -0500

    frist commit

---
+++ src/main/java/co/edu/escuelaing/mathematicaloperation/MathematicaloperationApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.mathematicaloperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathematicaloperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathematicaloperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/escuelaing/mathematicaloperation/MathematicaloperationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.mathematicaloperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathematicaloperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
